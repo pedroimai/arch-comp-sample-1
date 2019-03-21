be1ad573e1bf9ff388f07a073212c90243679ff3
pedroimai <pedroimai90@gmail.com>
2019-03-21 15:03:14 -0300

    config fix

--- app/src/main/java/com/pedroimai/archcomp/AppShell.java
+++ app/src/main/java/com/pedroimai/archcomp/AppShell.java
@@ -4,7 +4,7 @@ import com.facebook.buck.android.support.exopackage.ExopackageApplication;
 
 public class AppShell extends ExopackageApplication {
     public AppShell() {
-        super(BuildConfig.EXOPACKAGE_FLAGS);
+        super(BuildConfig.EXOPACKAGE_FLAGS == 1);
 
     }
 }
